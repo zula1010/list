8a87991a66504f049b6d3d0bb6d42a6f28879b68
Munkhzul Khurelbaatar <julia@Munkhzuls-MacBook-Pro.local>
2019-08-09 12:05:19 -0700

    frontend removed

--- backend/app.js
+++ backend/app.js
@@ -1,47 +1,57 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const cors = require('cors')
+const session = require('express-session');
 
 const app = express()
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
-// app.use(cors)
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
-let db = [
-    {
-        "code": "Julia",
-        "data": ["a", "b", "c"]
-    }
-]
+app.use(session({ secret: "Julia" }));
 
-app.get('/', (req, res) => {
+let db = {};
 
+
+
+app.get('/', async (req, res) => {
     console.log("get huselt irev")
     if (req.query.code) {
-        for (let i = 0; i < db.length; i++) {
-            if (db[i].code == req.query.code) {
-                // console.log(db[i])
-                res.json(db[i])
-            }
-        }
+        req.session.code = req.query.code
+        res.redirect('/')
     }
+    if (!req.session.code) {
+        code = ""
+        var letters = "0123456789ABCDEF";
+        for (var i = 0; i < 32; i++)
+            code += letters[(Math.floor(Math.random() * 16))];
+        req.session.code = code
+
+    }
+    console.log(req.session)
+    if (!(req.session.code in db)) {
+        console.log("to create new db ")
+        db[req.session.code] = { name: req.session.code, data: [] }
+    }
+    console.log(db[req.session.code])
+    let html = '<html><body><div><h1>Backbar Recruiting</h1><h3>Your code:'
+    html += db[req.session.code].name + '</h3><ul>'
+
+
+    for (let i = 0; i < db[req.session.code].data.length; i++) {
+        let li = '<li>Data: ' + db[req.session.code].data[i] + '</li>'
+        html += li
+    }
+    html += '</ul>'
+    html += "<form method = 'post'><input name='val' type='text'/><button type='submit'>Save entry</button></form>"
+    html += '</div></body></html>'
+    await res.send(html)
+
 })
 app.post('/', (req, res) => {
     console.log("post huselt irev")
     if (req.body) {
         console.log(req.body)
-        for (let i = 0; i < db.length; i++) {
-            if (db[i].code == req.body.code) {
-                db[i].data.push(req.body.addValue)
-                console.log(db[i])
-                res.json(db[i])
-            }
-        }
+        db[req.session.code].data.push(req.body.val)
     }
+    res.redirect('/')
 })
 
 app.listen(3030, () => console.log("app is working on port 3030"))
--- frontend/src/Items.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const items = (props) => {
-    return (
-        <p>Data: {props.data}</p>
-    );
-}
-
-export default items;
